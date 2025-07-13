20f34d2af73acdbeee942213fbf1147473ce97cf
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-07-13 12:18:52 -0300

    [200~d/GitHub projetos/Python-mvc-Rocketseat (main)

---
+++ ex_pylint.py
@@ -0,0 +1,10 @@
+
+print("Ola Mundo")
+
+def minha_funcao():
+    print("Estou na minha funcao")
+
+
+# snake_case -> Variaveis e funções em Python: my_variable
+# camelCase -> minhaFuncao
+# PascalCase -> Classe em Python
