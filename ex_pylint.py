679381dd0382b85d46ce684ba2b1ff8244db1a81
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-07-13 12:45:12 -0300

    config:Adicionando pre-commit e corrigindo problemas do pylint

--- ex_pylint.py
+++ ex_pylint.py
@@ -1,10 +1,27 @@
-
+"""
+Este é um módulo de exemplo para demonstrar correções de Pylint.
+Ele contém uma função simples e um exemplo de comentários sobre convenções de nomenclatura.
+"""
 print("Ola Mundo")
 
 def minha_funcao():
+    """
+    Esta função imprime uma mensagem indicando que está sendo executada.
+    É um exemplo simples de uma função em Python.
+    """
     print("Estou na minha funcao")
 
 
 # snake_case -> Variaveis e funções em Python: my_variable
 # camelCase -> minhaFuncao
 # PascalCase -> Classe em Python
+
+# Se você quiser descomentar esta função, renomeie-a para seguir a convenção snake_case:
+# def minha_funcao_com_erro():
+#     """
+#     Esta é uma função de exemplo que foi corrigida para seguir as convenções de nomenclatura.
+#     """
+#     print("Estou na minha funcao com erro")
+
+# Garanta que esta seja a última linha do arquivo.
+# Pylint espera que o arquivo termine com uma única nova linha.
