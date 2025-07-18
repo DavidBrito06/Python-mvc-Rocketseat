8cd300ce3ddf1d964e089b374944f28397c29c89
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-07-17 21:38:30 -0300

    feat: add PetDeleteController and corresponding test for deleting pets

---
+++ src/controllers/pet_deletet_controller_test.py
@@ -0,0 +1,8 @@
+from src.controllers.pet_deletet_controller import PetDeleteController
+
+def test_delete_pet(mocker):
+    mock_repository = mocker.Mock()
+    controller = PetDeleteController(mock_repository)
+    controller.delete_pet("Buddy")
+
+    mock_repository.delete_pet.assert_called_once_with("Buddy")
